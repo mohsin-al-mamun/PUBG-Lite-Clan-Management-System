07f26006893864d4ac44783fdbcb3d4ab8853d52
Mohsin <oxgmohsin@gmail.com>
2021-04-04 18:52:00 +0600

    JASON,localstorage and a Reload button added

--- main.js
+++ main.js
@@ -9,8 +9,9 @@ let Add     = document.getElementById("inputbtn");
 //DOM Nodes....... Showcase Section
 let ShowcaseTable = document.getElementById("showcaseTable");
 
-//DOM Nodes....... Analyze Button.
+//DOM Nodes....... Analyze and Reload Button.
 let Analyze = document.getElementById("analyze");
+let Reload = document.getElementById('reload');
 
 //DOM Nodes....... OutPut  Section.
 let LeaderTable = document.getElementById("leaderTable");
@@ -18,6 +19,10 @@ let CoLeaderTable = document.getElementById('coLeaderTable');
 let Top13TAble = document.getElementById('top13Table');
 
 
+// on load section.
+
+// local storage section.
+let Players = JSON.parse(localStorage.getItem("myjason"));
 
 
 
@@ -25,30 +30,60 @@ let Top13TAble = document.getElementById('top13Table');
 
 
 // Empty Array for randomly inputed Players.
-let Players=[];
+// let Players=[];
 
 
 
 // Event Listeners...........
         //counter vsriables
+                let onloadRow = 1;
                 let row = 1;
                 let scroll = 30;
                 let leader_count = 1;
                 let co_leader_count = 1;
                 let tpo13_count = 1;
 
+window.onload = function(){
+    if (Players){
+        ShowcaseTable.style.visibility = "visible";
+        for (let i=0; i<Players.length; i++){
+              // Populating the Showcase Table...
+        let newRow = ShowcaseTable.insertRow(onloadRow); // Adding a new row.
+
+        // creating cell for the new row.
+           let cell1 = newRow.insertCell(0); 
+           let cell2 = newRow.insertCell(1);
+           let cell3 = newRow.insertCell(2);
+
+
+       //  cell's value...
+           cell1.innerText = Players[i].name;
+           cell2.innerText = Players[i].points;
+           cell3.innerText = Players[i].status;
+
+           onloadRow++
+        };
+       
+    }else{
+        return ;};
+};
+
+
 Add.addEventListener("click",function (){
+     
+    if (Players == null){
+        Players = [];
+    };
+
+
     if( Name.value && Point.value && Status.value){
         // alert("It's working!!!!!");
         ShowcaseTable.style.visibility = "visible"; // This will visible the table.
       
         // populating the Array...
         playerObject={name: Name.value, points:Point.value, status:Status.value };
-        Players.push(playerObject);
         
-
-
-
+        
 
         // Populating the Showcase Table...
         let newRow = ShowcaseTable.insertRow(row); // Adding a new row.
@@ -64,6 +99,8 @@ Add.addEventListener("click",function (){
         cell2.innerText = Point.value;
         cell3.innerText = Status.value;
 
+        Players.push(playerObject);
+
     window.scrollBy(0,scroll);
 
     row++
@@ -75,7 +112,10 @@ Add.addEventListener("click",function (){
 
     }else{
         alert(" Opps!!!, Perhaps You missed to enter some inputs.");
-    }
+    };
+
+    localStorage.setItem('myjason', JSON.stringify(Players));
+    Players = JSON.parse(localStorage.getItem('myjason'));
 });
 
 
@@ -88,7 +128,7 @@ Add.addEventListener("click",function (){
 
 // // Leader's Table..
 Analyze.addEventListener('click',function(){
-
+    
     let n = Players.length;
     if (n==0){alert(" No data is added yet!!!.")};
     for (let i=0; i<n; i++){
@@ -106,6 +146,9 @@ Analyze.addEventListener('click',function(){
             
         };
     };
+
+    Reload.style.visibility = "visible";
+    Analyze.style.visibility = "hidden";
 });
 
 
@@ -143,28 +186,37 @@ Analyze.addEventListener('click', function(){
     console.log(Players); // for debuging purpose.
     let n = Players.length;
 
+    
+
     for(let i=0; i<n; i++){
 
-            if (tpo13_count>13){
-                alert("The datas are already analyzed. Please reload the page for new input.");
-                break;
-            };
+        if (tpo13_count>13){
+            alert("The datas are already analyzed. Please reload the page for new input.");
+            break;
+        };
 
-            let newRow = Top13TAble.insertRow(tpo13_count);
+        let newRow = Top13TAble.insertRow(tpo13_count);
 
-            let cell1 = newRow.insertCell(0);
-            let cell2 = newRow.insertCell(1);
-            let cell3 = newRow.insertCell(2);
+        let cell1 = newRow.insertCell(0);
+        let cell2 = newRow.insertCell(1);
+        let cell3 = newRow.insertCell(2);
 
-           cell1.innerText = Players[i].name;
-           cell2.innerText = Players[i].points;
-           cell3.innerText = Players[i].status;
+        cell1.innerText = Players[i].name;
+        cell2.innerText = Players[i].points;
+        cell3.innerText = Players[i].status;
 
-           tpo13_count++;
-        
+        tpo13_count++;
+
     };
 
-    window.scrollBy(0,300);
+    
+    
+    window.scrollBy(0,200);
+});
+
+Reload.addEventListener('click', function(){
+    Reload.style.visibility = 'hidden';
+    location.reload();
 });
 
 
